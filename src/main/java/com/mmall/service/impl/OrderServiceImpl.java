2f16bcade6802e0cf429ea5d958a016519744dc5
Yang Zhang <30611935+YangZhangHaveFun@users.noreply.github.com>
2019-06-20 11:19:59 +1000

    Update OrderServiceImpl.java

--- src/main/java/com/mmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/mmall/service/impl/OrderServiceImpl.java
@@ -48,7 +48,11 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by geely
