bf7d5743fed933c4f3eac8bc8dbf951e736eccd1
Yang Zhang <30611935+YangZhangHaveFun@users.noreply.github.com>
2019-07-08 22:32:58 +1000

    temp fix

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -22,7 +22,7 @@ public class CookieUtil {
         if(cks != null){
             for(Cookie ck : cks){
                 log.info("read cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
-                if(StringUtils.equals(ck.getName(),COOKIE_NAME)){
+                if(StringUtils.equals(ck.getName(),COOKIE_NAME )|| StringUtils.equals(ck.getName(),"JSESSIONID")){
                     log.info("return cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
                     return ck.getValue();
                 }
