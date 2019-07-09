a9c817aa82dcc6dd91177751c574735d5d4e201f
Yang Zhang <30611935+YangZhangHaveFun@users.noreply.github.com>
2019-07-09 13:15:30 +1000

    update domain name

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
 
-    private final static String COOKIE_DOMAIN = ".happymmall.com";
+    private final static String COOKIE_DOMAIN = ".yzzhan.info";
     private final static String COOKIE_NAME = "mmall_login_token";
 
 
@@ -22,7 +22,7 @@ public class CookieUtil {
         if(cks != null){
             for(Cookie ck : cks){
                 log.info("read cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
-                if(StringUtils.equals(ck.getName(),COOKIE_NAME )|| StringUtils.equals(ck.getName(),"JSESSIONID")){
+                if(StringUtils.equals(ck.getName(),COOKIE_NAME )){
                     log.info("return cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
                     return ck.getValue();
                 }
