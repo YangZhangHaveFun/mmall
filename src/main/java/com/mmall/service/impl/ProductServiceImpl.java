bc75b547425a08964172fd2e81e0211a4926c0ed
Yang Zhang <30611935+YangZhangHaveFun@users.noreply.github.com>
2019-09-15 02:28:56 +1000

    Update ProductServiceImpl.java

--- src/main/java/com/mmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ProductServiceImpl.java
@@ -108,7 +108,7 @@ public class ProductServiceImpl implements IProductService {
         productDetailVo.setStatus(product.getStatus());
         productDetailVo.setStock(product.getStock());
 
-        productDetailVo.setImageHost(PropertiesUtil.getProperty("ftp.server.http.prefix","http://img.happymmall.com/"));
+        productDetailVo.setImageHost("http://img.happymmall.com/");
 
         Category category = categoryMapper.selectByPrimaryKey(product.getCategoryId());
         if(category == null){
